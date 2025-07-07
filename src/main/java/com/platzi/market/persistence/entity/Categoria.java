dea01afad34cb2eff4a84e9361e163bb6dd4c097
Einarr07 <aidenperse@gmail.com>
2025-07-07 15:42:05 -0500

    Implementación del mapeo de tablas con JPA
    - Se crearón todas las tablas con su respectivo mapeo que se van a realizar
    dentro del curso estas se allán dentro de la capa de persistencia.
    
    - pom.xml: Se agrego una nueva dependencia "Lombok"

---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,25 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+@Getter @Setter
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCategoria")
+    private Long idCategoria;
+
+    private String descripcion;
+
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+}
---
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,31 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+@Getter @Setter
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Double celular;
+
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+}
